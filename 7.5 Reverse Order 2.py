d04330bf27739536b42dc69476d821a23209ef69
timursdet <timursdet@gmail.com>
2024-01-18 03:14:41 +0300

    test change for commit

--- 7.5 Reverse Order 2.py
+++ 7.5 Reverse Order 2.py
@@ -1,7 +1,8 @@
 num = int(input())
-new_num = str('')
+new_num = str("")
 while num != 0:
     last_digit = num % 10
     new_num = new_num + str(last_digit)
     num = num // 10
+print(new_num)
 print(new_num)
